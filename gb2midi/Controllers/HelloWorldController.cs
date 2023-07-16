79e26b079f90a1399e2e87cb947d9d32586b5791
Numen Rubino <numenrr@gmail.com>
2023-07-16 12:51:42 -0500

    initial commit 2

---
+++ gb2midi/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace gb2midi.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public string Index()
+    {
+        return "This is my default action...";
+    }
+    // 
+    // GET: /HelloWorld/Welcome/
+    public string Welcome(string name, int ID = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+    }
+    //public string Welcome(String name, int times=1)
+    //{
+    //    return HtmlEncoder.Default.Encode($"hello {name}, times var is: {times}");
+    //    //return "This is the Welcome action method...";
+    //}
+}
---
+++ gb2midi/GbReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace gb2midi
+{
+	public class GbReader
+	{
+		private StreamReader streamReader;
+		public GbReader(Stream inStream)
+		{
+			this.streamReader = new StreamReader(inStream);
+		}
+
+		private void ReadFromStream()
+		{
+			string line;
+            while ((line = streamReader.ReadLine()) != null)
+			{
+
+			}
+        }
+	}
+}
+
